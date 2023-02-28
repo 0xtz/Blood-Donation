c01c2b4f5b6ac6adc5338dbb51d58362bd63bbc0
0xtz <0xtz.52@gmail.com>
2023-02-28 14:40:58 +0100

    🐛 FIX: no changes 😂

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,5 +1,8 @@
 // import { Provider } from "react-redux";
 // import { store } from "@/store/store";
+import { Session } from "next-auth";
+import { headers } from "next/headers";
+// import the context
 
 export const metadata = {
   title: "BD | User",
@@ -11,6 +14,11 @@ export default function UserLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // get the session from the local storage
+  const session = JSON.parse(
+    // @ts-ignore
+    headers["next-auth.session-token"]
+  ) as Session;
   return (
     // <Provider store={store}>
     <html lang="en">
