f65723514953dfa91182847830d253b4a0deb6b3
Taha ziani <53442065+0xtz@users.noreply.github.com>
2023-03-03 02:52:56 +0100

    export removed

--- app/(public)/faq/page.tsx
+++ app/(public)/faq/page.tsx
@@ -3,7 +3,7 @@ import { use } from "react";
 import prisma from "@/lib/prismadb";
 import UserForm from "./UserForm";
 
-export async function listHospitals()
+async function listHospitals()
 {
 const hospitals = await prisma.hospital.findMany({
     select: {
