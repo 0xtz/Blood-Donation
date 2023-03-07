6157102865d18d35c7b88b6e2ad0e8671b0d5652
Ouma <oumaimaelkourti082@gmail.com>
2023-03-07 10:14:48 +0100

    no changes

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "pages/api/auth/[...nextauth]";
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "pages/api/auth/[...nextauth]";
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
+  return <pre>
+
+  </pre>;
 }
 
 // visit http://localhost:3000/admin/dashboard
